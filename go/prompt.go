ad22edbc80b18d1ea5ca3d6bbc6044f1a3bb32bf
yagi <hiro.yagi0245@gmail.com>
2017-01-23 20:26:12 +0900

    add eval

--- go/prompt.go
+++ go/prompt.go
@@ -21,24 +21,44 @@ func display(p *lispy.Parser, line string) {
 }
 
 func repl() {
+	var line string
+	var err error
+	var contFlag bool
 	fmt.Println("Lispy Version 0.0.0.0.1")
 	fmt.Println("Press Ctrl+c to Exit")
 	rl, err := readline.New("lispy> ")
 	if err != nil {
 		panic(err)
 	}
+
 	p := lispy.Parser{}
+
 	for {
-		line, err := rl.Readline()
+		tmpline, err := rl.Readline()
+		if contFlag {
+			line = line + "\n" + tmpline
+			contFlag = false
+			rl.SetPrompt("lispy> ")
+		} else {
+			line = tmpline
+		}
 		if err != nil {
 			break
 		}
 		display(&p, line)
 		program, err := p.ParseString(line)
 		if err != nil {
-			fmt.Println(err.Error())
-			continue
+			switch err.(type) {
+			case *lispy.UnclosedError:
+				contFlag = true
+				rl.SetPrompt("... ")
+				continue
+			default:
+				fmt.Println(err.Error())
+				continue
+			}
 		}
+
 		for _, ast := range program {
 			fmt.Println(ast)
 		}
