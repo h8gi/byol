44bcbd549992a78a3435b46393c5a5d829186856
yagi <hiro.yagi0245@gmail.com>
2017-01-28 02:35:29 +0900

    implement Eq

--- go/secd-test.go
+++ go/secd-test.go
@@ -10,13 +10,13 @@ func main() {
 	symb := lispy.NewSymbol("b")
 	list := lispy.NewList(syma, symb, syma)
 	fmt.Println(list)
-	fmt.Println(list.Eq(&list))
+	fmt.Println(list.Eq(list))
 	list2 := list
-	fmt.Println(list.Eq(&list2))
-	fmt.Println(lispy.LispNull.Eq(&lispy.LispNull))
-	fmt.Printf("%v eq %v = %v\n", syma, symb, syma.Eq(&symb))
-	fmt.Printf("%v eq %v = %v\n", syma, syma, syma.Eq(&syma))
+	fmt.Println(list.Eq(list2))
+	fmt.Println(lispy.LispNull.Eq(lispy.LispNull))
+	fmt.Printf("%v eq %v = %v\n", syma, symb, syma.Eq(symb))
+	fmt.Printf("%v eq %v = %v\n", syma, syma, syma.Eq(syma))
 	syma2 := lispy.NewSymbol("a")
-	fmt.Printf("%v eq %v = %v\n", syma, syma2, syma.Eq(&syma2))
+	fmt.Printf("%v eq %v = %v\n", syma, syma2, syma.Eq(syma2))
 
 }
