91d65c3d264f6be1c4aff7f0e20d50f9f3ceeba9
yagi <hiro.yagi0245@gmail.com>
2017-01-28 02:31:55 +0900

    add symbol intern

--- go/secd-test.go
+++ go/secd-test.go
@@ -6,7 +6,17 @@ import (
 )
 
 func main() {
-	syma, _ := lispy.NewSymbol("a")
-	symb, _ := lispy.NewSymbol("b")
-	fmt.Println(lispy.NewList(syma, symb, symb))
+	syma := lispy.NewSymbol("a")
+	symb := lispy.NewSymbol("b")
+	list := lispy.NewList(syma, symb, syma)
+	fmt.Println(list)
+	fmt.Println(list.Eq(&list))
+	list2 := list
+	fmt.Println(list.Eq(&list2))
+	fmt.Println(lispy.LispNull.Eq(&lispy.LispNull))
+	fmt.Printf("%v eq %v = %v\n", syma, symb, syma.Eq(&symb))
+	fmt.Printf("%v eq %v = %v\n", syma, syma, syma.Eq(&syma))
+	syma2 := lispy.NewSymbol("a")
+	fmt.Printf("%v eq %v = %v\n", syma, syma2, syma.Eq(&syma2))
+
 }
