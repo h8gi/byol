e22a43af3f2a0860b5e37c0ba36a22ea2e5cc359
yagi <hiro.yagi0245@gmail.com>
2017-01-28 01:09:23 +0900

    add NewList

--- go/secd-test.go
+++ go/secd-test.go
@@ -6,16 +6,7 @@ import (
 )
 
 func main() {
-	syma, _ := lispy.NewSymbol("A")
-	symb, _ := lispy.NewSymbol("B")
-	env := lispy.NewEnv()
-	env.Define(syma, symb)
-	env.Define(symb, syma)
-	env.Define(symb, symb)
-	fmt.Println(env)
-	result, err := env.LookUp(syma)
-	fmt.Println(result, err)
-	result, err = env.LookUp(symb)
-	fmt.Println(result, err)
-	fmt.Println(lispy.InitialEnv())
+	syma, _ := lispy.NewSymbol("a")
+	symb, _ := lispy.NewSymbol("b")
+	fmt.Println(lispy.NewList(syma, symb, symb))
 }
