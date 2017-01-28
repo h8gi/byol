d3362da8de816827a077fe9cd331e8ef1d5a438d
yagi <hiro.yagi0245@gmail.com>
2017-01-28 19:59:15 +0900

    rename data type flag

--- go/secd-test.go
+++ go/secd-test.go
@@ -18,5 +18,21 @@ func main() {
 	fmt.Printf("%v eq %v = %v\n", syma, syma, syma.Eq(syma))
 	syma2 := lispy.NewSymbol("a")
 	fmt.Printf("%v eq %v = %v\n", syma, syma2, syma.Eq(syma2))
-
+	str1 := lispy.LObj{Type: lispy.DTString, Value: "FOO"}
+	str2 := lispy.LObj{Type: lispy.DTString, Value: "FOO"}
+	fmt.Printf("%v eq %v = %v\n", str1, str2, str1.Eq(str2))
+	list3 := lispy.NewList(syma, symb, syma)
+	fmt.Printf("%v eq %v = %v\n", list, list3, list.Eq(list3))
+	a := lispy.Cons(syma, symb)
+	b := lispy.Cons(syma, symb)
+	fmt.Printf("%v eq %v = %v\n", a, b, a.Eq(b))
+	fmt.Printf("%v eq %v = %v\n", a, a, a.Eq(a))
+	c := a
+	fmt.Printf("%v eq %v = %v\n", a, c, a.Eq(c))
+	fmt.Println(lispy.NewVector(syma, syma, symb, syma))
+	fmt.Println(list.Length())
+	fmt.Println(list)
+	list.Pop()
+	fmt.Println(list)
+	fmt.Println(list2)
 }
